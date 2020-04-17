eeba9dee3437f810f078539fadc87b17b5638ef8
AslihanArslan <arslanaslihan01@gmail.com>
2020-04-17 22:34:21 +0300

    add csv file

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+import csv
+import pandas as pd
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    user = {'username': 'Miguel'}
-    return render_template('dashboard.html', title='Home', user=user)
+    df = pd.read_csv('./data/COVID.csv', index_col=False)
 
+    s_array = df["Günlük Vaka Sayısı"]
+    print(s_array[0])
+    return render_template('dashboard.html', title='Home')
+    
 if __name__ == "__main__":
     app.run(debug=True)
+
+
