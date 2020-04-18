8a00f3b46c847d5497617a696b8b7432b971b2ac
UgurOzcanFited <ugur@fited.co>
2020-04-18 19:01:13 +0300

    update csv

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     df = pd.read_csv('./data/COVID.csv', index_col=False)
-    map(lambda x: x if x != '-' else '0', df)
-    titles = list(df.columns.values) 
+    #map(lambda x: x if x != '-' else '0', df)
+    titles = list(df.columns.values)
     dates = df[df.columns[0]].tolist()
     gvs = df[df.columns[1]].tolist()
     tvs = df[df.columns[2]].tolist()
@@ -17,12 +17,12 @@ def hello():
     gvs2 = df[df.columns[5]].tolist()
     tvs2 = df[df.columns[6]].tolist()
     gts = df[df.columns[7]].tolist()
-    gts = list(map(lambda b: b.replace("-","0"), gts))
+    #gts = list(map(lambda b: b.replace("-","0"), gts))
     tts = df[df.columns[8]].tolist()
-    tts = list(map(lambda b: b.replace("-","0"), tts))
+    #tts = list(map(lambda b: b.replace("-","0"), tts))
+
     return render_template('dashboard.html', dates=dates, gvs=gvs, tvs=tvs, gis=gis, tis=tis, gvs2=gvs2, tvs2=tvs2, gts=gts, tts=tts, titles=titles)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
